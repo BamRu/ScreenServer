e7fb1b37f68b27be601a0852cbdf29ee9f29b8e6
BamRu <is641ryjkov@mail.ru>
2021-04-03 17:15:50 +0700

    Work lab

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+app.use('/image', express.static(path.join(__dirname,'..','image')));
 
 app.get('/', (req, res) => {
-	express.static()
 	res.sendfile('index.html');
 })
+
+app.get('/test', (req,res) => {
+	res.sendfile('image/1.png');
+})	
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
+
