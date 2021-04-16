0127ef8c97c25a378929e0fcd2902f2a8b9b78a8
BamRu <is641ryjkov@mail.ru>
2021-04-17 01:43:49 +0700

    123

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { createCanvas } = require('canvas')
 const path = require('path');
 const app = express();
 const port = 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use("/node_modules", express.static('./node_modules/'));
 
 app.use('/test', bodyParser.urlencoded({
     extended: true
@@ -15,8 +17,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/test', (req,res) => {
-	console.log('Uploaded: ', req.file)
+	console.log('Uploaded: ', req.body.photoF);
 	// Homework: Upload file to S3
+	var imgf = new Buffer(req.body.photoF, 'base64');
+	var imgb = new Buffer(req.body.photoB, 'base64');
+	
 })	
 
 app.listen(port, () => {
