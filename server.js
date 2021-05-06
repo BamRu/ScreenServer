fe2aabdf3d75665debfcac7e54ae7237eed99332
BamRu <is641ryjkov@mail.ru>
2021-05-06 11:42:14 +0700

    123

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
-const multer  = require("multer"); // для сохранения фалов
-const upload = multer({ dest: 'uploads/' })
+const mergeImages = require('merge-images');
+const { Canvas, Image } = require('canvas');
 
-const bodyParser = require('body-parser'); // обработка форм
-const urlencodedParser = bodyParser.urlencoded({extended: false}); // для обработки формы
+const multer  = require("multer"); // для сохранения фалов
+const upload = multer({ dest: 'uploads/' });
 
 const reduce = require('image-blob-reduce')();	// blob to canvas
-const { createCanvas, Image} = require('canvas');	// canvas
+
 // const canvasf = createCanvas(300, 150);	//	canvas 1
 // const canvasb = createCanvas(300, 150);	//	canvas 2
 // const ctxf = canvasf.getContext('2d');	//	canvas
 // const ctxb = canvasb.getContext('2d');	//	canvas
 
-const imagef = new Image();
-const imageb = new Image();
+// const imagef = new Image();
+// const imageb = new Image();
 
 app.use(express.static(path.join(__dirname, 'public')));	
 app.use("/node_modules", express.static('./node_modules/'));
@@ -33,26 +33,33 @@ app.get('/', (req, res) => {
 //app.post("/test", urlencodedParser, function (req, res){
 app.post("/test", upload.any(), function (req, res) {
 	
-	console.log(req.body);
-	// console.log(req.body.photo);
-	console.log(req.files);
-	console.log(req.files[0]);
+const img = new Image;	
+ 
+mergeImages([req.files[0].path, req.files[1].path], {
+  Canvas: Canvas,
+  Image: Image
+})
+  .then(b64 => img.src = b64);
 
-	let canvasf;
-	let canvasb;
+	// console.log(req.files[0]);
 
+	// let canvasf = reduce.toCanvas(req.files[0].path, 300);
+	// let canvasb = reduce.toCanvas(req.files[1].path, 300);
+
+	console.log(img.src);
+	// res.send(iamgef);
 })	
 
 
-app.post("/upload", function (req, res) {
+// app.post("/upload", function (req, res) {
    
-    let filedata = req.body;
-    console.log(filedata);
-    if(!filedata)
-        res.send("Ошибка при загрузке файла");
-    else
-        res.send("Файл загружен");
-})
+//     let filedata = req.body;
+//     console.log(filedata);
+//     if(!filedata)
+//         res.send("Ошибка при загрузке файла");
+//     else
+//         res.send("Файл загружен");
+// })
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
