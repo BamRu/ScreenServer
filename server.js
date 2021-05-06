e261ebe6a79b9e702ce5d42bfe14b5c079f1e1f0
bamru <is641ryjkov@mail.ru>
2021-05-06 13:52:13 +0700

    123

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 8000;
@@ -16,7 +17,7 @@ const img = new Image;
 app.use(express.static(path.join(__dirname, 'public')));	
 app.use("/node_modules", express.static('./node_modules/'));
 
-app.use("/upload", upload.any("filedata"));	// для сохранения используем пакет
+//app.use("/upload", upload.any("filedata"));	// для сохранения используем пакет
 
 
 app.get('/', (req, res) => {
@@ -27,19 +28,33 @@ app.get('/', (req, res) => {
 //app.post("/test", urlencodedParser, function (req, res){
 app.post("/test", upload.any(), function (req, res) {
 	
+	console.log(req.files[0]);
+	
+let path1 = "./"+ req.files[0].destination + req.files[0].filename;
+let path2 = "./"+ req.files[1].destination + req.files[1].filename;
 
+console.log(path1); 
+console.log(path2); 
+console.log(fs.readFileSync(path1, "ASCII"));
 
-mergeImages(["./image/2.png", "./image/1.png"], {
+mergeImages([base64ToPNG(fs.readFileSync(path1, "utf8")),base64ToPNG(fs.readFileSync(path2, "utf8"))],{
   Canvas: Canvas,
   Image: Image
 })
- .then(b64 => {img.src = b64});
+.then(b64 => {img.src = b64});
 
-	// console.log(req.files[0]);
 	console.log(img.src);
 	
 	res.send(img.src);
 	})	
+
+function base64ToPNG(data) {
+  data = data.replace(/^data:image\/png;base64,/, '');
+
+  fs.writeFile(path.resolve(__dirname, '../tmp/image.png'), data, 'base64', function(err) {
+    if (err) throw err;
+  });
+}
 
 
 // app.post("/upload", function (req, res) {
